Fixed reconstruction process when plane z=0 was out of range; added cnr logo; added structural system computation based on the optical components; added part of the segmentation using otsu method (not working one hundred percente); changes in the index page.

app/controllers/holo_reconstruction.py
@@ -33,6 +33,7 @@ def spatial_filter():
         z = np.arange(zstart, zend, step)
 
         attrlist = {'pitch': pitch, 'lenght': lenght}
+        
         hologram_metadata = session.get('hologram_metadata')
         hologram_metadata.update_attrs(attrlist)
         session['hologram_metadata'] = hologram_metadata
@@ -180,8 +181,7 @@ def reconstruct():
         step = float(request.form.get('step'))
 
         z = np.arange(zstart, zend, step)
-        print(z)
-
+        
         attrlist = {'pitch': pitch, 'lenght': lenght}
         hologram_metadata = session.get('hologram_metadata')
         hologram_metadata.update_attrs(attrlist)
@@ -190,8 +190,10 @@ def reconstruct():
         holo_ = data_grid(
             hologram_metadata.dataArray.data,
             spacing=hologram_metadata.dataArray.attrs['pitch'],
-            medium_index=1,
+            medium_index=1.333,
             illum_wavelen=hologram_metadata.dataArray.attrs['lenght'])
+        
+        #med_wavelen = holo_.illum_wavelen / holo_.medium_index
         r, fft = hp.propagate(holo_, z)
 
         hologram_metadata = session.get('hologram_metadata')

app/controllers/utils.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import random
 import numpy as np
 import cv2
+import math
 from app import app
 plt.switch_backend('Agg')
 
@@ -55,7 +56,6 @@ def autofocusing(r):
     tc = tc*rho
     best_focus = np.where(tc == np.max(np.max(tc)))
     best_focus = np.squeeze(best_focus)
-    print(best_focus)
     return best_focus
 
 
@@ -84,6 +84,10 @@ def save_images(r, fft, path_directory):
                 pad_inches=0)
     amp_img = 'reconstructed_amplitude' + str(rand_number) + '.png'
 
+    ang = np.angle(r[best_focus].data[:, :]) < -math.pi
+    ang = ang > math.pi
+    print(ang.any())
+    
     fig = plt.figure()
     plt.imshow(np.angle(r[best_focus].data[:, :]), cmap='gray')
     plt.axis('off')